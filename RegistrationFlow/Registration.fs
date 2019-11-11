6fea9940ed8255e89cd84310be17392cb3695411
Mark Seemann <mark@ploeh.dk>
2019-11-11 13:43:01 +0100

    Initialise project with sample code
    The sample code is taken from
    https://blog.ploeh.dk/2017/02/02/dependency-rejection/#ade3787e6e3c4e569854e2c2bd038e29
    Additional types are inferred from that sample.
    
    I've also formatted the code consistently with a four-space-per-tab
    indentation.

---
+++ RegistrationFlow/Registration.fs
@@ -0,0 +1,31 @@
+﻿module Ploeh.Samples.Registration
+
+open System
+
+type Mobile = Mobile of int
+type ProofId = ProofId of Guid
+type Registration = { Mobile : Mobile }
+type CompleteRegistrationResult = ProofRequired of ProofId | RegistrationCompleted
+
+let completeRegistrationWorkflow
+    (createProof: Mobile -> Async<ProofId>)
+    (verifyProof: Mobile -> ProofId -> Async<bool>)
+    (completeRegistration: Registration -> Async<unit>)
+    (proofId: ProofId option)
+    (registration: Registration)
+    : Async<CompleteRegistrationResult> =
+    async {
+        match proofId with
+        | None ->
+            let! proofId = createProof registration.Mobile
+            return ProofRequired proofId
+        | Some proofId ->
+            let! isValid = verifyProof registration.Mobile proofId
+            if isValid then
+                do! completeRegistration registration
+                return RegistrationCompleted
+            else
+                let! proofId = createProof registration.Mobile
+                return ProofRequired proofId
+    }
+
