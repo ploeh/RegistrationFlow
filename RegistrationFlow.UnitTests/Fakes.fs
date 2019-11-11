dc370cd2706e80c2336319ae07480069f7fa4fe4
Mark Seemann <mark@ploeh.dk>
2019-11-11 19:30:15 +0200

    Add characterisation test for missing proof ID

---
+++ RegistrationFlow.UnitTests/Fakes.fs
@@ -0,0 +1,34 @@
+﻿module Ploeh.Samples.Tests.Fakes
+
+open System
+open Ploeh.Samples.Registration
+open System.Collections.ObjectModel
+
+type Fake2FA () =
+    let mutable proofs = Map.empty
+
+    member _.CreateProof mobile =
+        match Map.tryFind mobile proofs with
+        | Some (proofId, _) -> proofId
+        | None ->
+            let proofId = ProofId (Guid.NewGuid ())
+            proofs <- Map.add mobile (proofId, false) proofs
+            proofId
+        |> fun proofId -> async { return proofId }
+
+    member _.VerifyProof mobile proofId =
+        match Map.tryFind mobile proofs with
+        | Some (_, true) -> true
+        | _ -> false
+        |> fun b -> async { return b }
+
+    member _.VerifyMobile mobile =
+        match Map.tryFind mobile proofs with
+        | Some (proofId, _) ->
+            proofs <- Map.add mobile (proofId, true) proofs
+        | _ -> ()
+
+type FakeRegistrationDB () =
+    inherit Collection<Registration> ()
+
+    member this.CompleteRegistration r = async { this.Add r }
---
+++ RegistrationFlow.UnitTests/RegistrationFacadeTests.fs
@@ -0,0 +1,30 @@
+﻿module Ploeh.Samples.RegistrationFacadeTests
+
+open Xunit
+open Swensen.Unquote
+open Ploeh.Samples.Tests.Fakes
+open Ploeh.Samples.Registration
+
+let createFixture () =
+    let twoFA = Fake2FA ()
+    let db = FakeRegistrationDB ()
+    let sut =
+        completeRegistrationWorkflow
+            twoFA.CreateProof
+            twoFA.VerifyProof
+            db.CompleteRegistration
+    sut, twoFA, db
+
+[<Theory>]
+[<InlineData 123>]
+[<InlineData 432>]
+let ``Missing proof ID`` mobile = async {
+    let sut, twoFA, db = createFixture ()
+    let r = { Mobile = Mobile mobile }
+
+    let! actual = sut None r
+
+    let! expectedProofId = twoFA.CreateProof r.Mobile
+    let expected = ProofRequired expectedProofId
+    expected =! actual
+    test <@ Seq.isEmpty db @> }
